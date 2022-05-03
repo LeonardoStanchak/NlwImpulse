fe1452abb2d42df68f97d2838b8ff310f80171c5
LeonardoStanchak <leonardostanchak@hotmail.com>
2022-05-03 11:43:03 -0300

    WidgetForm finalizado

--- feedGet/web/src/components/WidgetForm.tsx
+++ feedGet/web/src/components/WidgetForm.tsx
@@ -1,7 +1,6 @@
 export function WidgetForm(){
     return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg ">
-            Ola mundo
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
         </div>
     );
 }
