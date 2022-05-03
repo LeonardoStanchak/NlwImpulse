91e6d86179f62a06bf035ae2771fedbbec24440f
LeonardoStanchak <leonardostanchak@hotmail.com>
2022-05-03 10:38:17 -0300

    Primeira Aula Finalizada

--- feedGet/web/src/components/Widget.tsx
+++ feedGet/web/src/components/Widget.tsx
@@ -1,15 +1,18 @@
 import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
+import { Popover } from '@headlessui/react';
 
 export function Widget() {
   return (
-    <div className="absolute bottom-5 right-5">
-      <button className="bg-brand-500 rounded-full rounded-full px-3 h-12 text-white flex items-center group">
+    <Popover className="absolute bottom-5 right-5">
+     <Popover.Panel>Qual o feedback ?</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-1000 ease-linear">
             <span className="pl-2"></span>
             Feedback
             </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
