b806eafe2132494e5ee3e3a9e1528c73292235cb
LeonardoStanchak <leonardostanchak@hotmail.com>
2022-05-03 15:16:37 -0300

    gitIgnore

--- feedGet/web/src/components/WidgetForm.tsx
+++ feedGet/web/src/components/WidgetForm.tsx
@@ -1,5 +1,17 @@
 import { CloseButton } from "./Closebutton";
 
+const feedback = {
+    BUG:{
+        title: 'Problema'
+    },
+    IDEA:{
+        title: 'Ideia'
+    },
+    OTHER:{
+        title: 'Outro'
+    },
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -9,7 +21,9 @@ export function WidgetForm() {
         <CloseButton/>
       </header>
 
-      <p>Ola meu mundost</p>
+      <div className="flex py-8 gap-2 w-full">
+
+      </div>
 
       <footer className="text-xl text-neutral-400">
           Feito com ♥ pela <a className="underline underline-offset-2" href="https://rockeseat.com.br"></a>
