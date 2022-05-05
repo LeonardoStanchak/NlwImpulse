2ee83f11d0e816e1cead01259e2fe1ca3f8e7602
LeonardoStanchak <leonardostanchak@hotmail.com>
2022-05-05 13:48:54 -0300

    Final do widget

--- feedGet/web/src/components/Steps/FeedbackContentStep.tsx
+++ feedGet/web/src/components/Steps/FeedbackContentStep.tsx
@@ -1,11 +1,15 @@
+import { ArrowLeft } from "phosphor-react";
 import { CloseButton } from "../CloseButton"
 import { FeedbackType, feedbackType } from "../WidgetForm"
 
 interface feedbackContentStepProps{
-    FeedbackType: FeedbackType
+    FeedbackType: FeedbackType;
+    onFeedbackRestartRequested: ()=>void;
 }
 
-export function FeedbackContentStep({FeedbackType}: feedbackContentStepProps) {
+export function FeedbackContentStep({FeedbackType,
+     onFeedbackRestartRequested
+    }: feedbackContentStepProps) {
      
     const feedbackTypeInfo = feedbackType[FeedbackType];
 
@@ -13,16 +17,27 @@ export function FeedbackContentStep({FeedbackType}: feedbackContentStepProps) {
         
         <>
         <header>
-        <span className="text-xl leading-6">
+            <button type="button"
+             className="top-5  left-5 absolute  text-zinc-400 hover:text-zinc-100"
+             onClick={onFeedbackRestartRequested}
+             >
+                <ArrowLeft weight="bold" className="w-4 h-4"/>
+            </button>
+
+            
+        <span className="text-xl leading-6 flex items-center gap-2">
             <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
             {feedbackTypeInfo.title}
         </span>
         <CloseButton />
       </header>
 
-    <div className="flex py-8 gap-2 w-full">
-    
-    </div>
+    <form className="my-4 w-full">
+        <textarea 
+        className=""
+        placeholder="Conte com detalhes oque está acontecendo..."
+        />
+    </form>
     </>
     
     )
--- feedGet/web/src/components/WidgetForm/index.tsx
+++ feedGet/web/src/components/WidgetForm/index.tsx
@@ -35,18 +35,17 @@ export type FeedbackType = keyof typeof feedbackType;
 
 export function WidgetForm() {
   const [feedbackTypes, setFeedbackTypes] = useState<FeedbackType | null>(null);
+    
+  function handleRestartFeedback(){
+      setFeedbackTypes(null);
+    }
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-
-        <CloseButton/>
-      </header>
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       
       {!feedbackTypes ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackTypes} />
       ) : (
-        <FeedbackContentStep FeedbackType={feedbackTypes} />
+        <FeedbackContentStep FeedbackType={feedbackTypes}  onFeedbackRestartRequested={handleRestartFeedback}/>
       )}
 
       <footer className="text-xl text-neutral-400">
