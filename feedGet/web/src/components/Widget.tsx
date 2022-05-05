dbee982034b34c7ebf0fb5c05c548efd5a1f4278
LeonardoStanchak <leonardostanchak@hotmail.com>
2022-05-05 15:43:38 -0300

    Finalização do Botão camera

--- feedGet/web/src/components/Widget.tsx
+++ feedGet/web/src/components/Widget.tsx
@@ -9,7 +9,7 @@ export function Widget() {
      <Popover.Panel>
        <WidgetForm />
      </Popover.Panel>
-      <Popover.Button className="bg-brand-500 rounded-full rounded-full px-3 h-12 text-white flex items-center group">
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-1000 ease-linear">
             <span className="pl-2"></span>
